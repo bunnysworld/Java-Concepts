63f9ab3a30564a3dcf026607f8efe9bab6d0d126
Akash Dwivedi <akashdwivedi563@gmail.com>
2024-09-06 21:22:56 +0530

    thread pool executor

---
+++ compP/src/concepts/threading/threadPool/ThreadPoolExecutorExample.java
@@ -0,0 +1,29 @@
+package concepts.threading.threadPool;
+
+import java.util.concurrent.*;
+
+public class ThreadPoolExecutorExample {
+    public static void main(String[] args) {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2, 4, 1,
+                TimeUnit.MINUTES, new ArrayBlockingQueue<>(2), new ThreadPoolExecutor.CallerRunsPolicy());
+        for (int i = 0; i < 10; i++) {
+            threadPoolExecutor.submit(() -> {
+                try {
+                    System.out.println("Task executed by thread " + Thread.currentThread().getName());
+                    Thread.sleep(2000);
+                } catch (Exception e) {
+
+                }
+            });
+        }
+
+    }
+}
+
+class CustomRejectionHandler implements RejectedExecutionHandler {
+    @Override
+    public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+        System.out.println("task rejected : " + r.toString());
+        ;
+    }
+}
