aa0f6ac81414735803b50913d9dbe09ecac97f98
Akash Dwivedi <akashdwivedi563@gmail.com>
2024-09-10 20:14:35 +0530

    java Future example

---
+++ compP/src/concepts/future/FutureExample.java
@@ -0,0 +1,35 @@
+package concepts.future;
+
+import java.nio.file.LinkOption;
+import java.util.concurrent.*;
+
+public class FutureExample {
+    public static void main(String[] args) {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 2,
+                TimeUnit.MINUTES, new ArrayBlockingQueue<>(1), Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy());
+        Future<?> future = executor.submit(() -> {
+            try {
+                Thread.sleep(7000);
+                System.out.println("Task is executed by thread : " + Thread.currentThread().getName());
+            } catch (Exception e) {
+
+            }
+        });
+        try {
+            future.get(2, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            System.out.println("Exception Task is not completed yet");
+        }
+        System.out.println("Is task done " + future.isDone());
+        System.out.println("Is Task Cancelled " + future.isCancelled());
+        try {
+            System.out.println("Waiting for task to finish");
+            future.get();
+            System.out.println("task is finished ..");
+        } catch (Exception e) {
+
+        }
+        System.out.println("After waiting is Task Done "+future.isDone());
+    }
+}
